ce88c58ad29ba4870168c0b52c0f75b7a7cae823
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-07-09 14:34:28 +0300

    Update Arithmetic-Operators.py

--- Python Challenges/Arithmetic-Operators.py
+++ Python Challenges/Arithmetic-Operators.py
@@ -5,4 +5,3 @@ The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
 '''
-
