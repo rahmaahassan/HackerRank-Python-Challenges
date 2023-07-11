b49d2fedb93f53d57db2a31bacfd65e5ff58b9cc
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-07-11 12:57:00 +0300

    Update What's-Your-Name?.py

--- Python Challenges/What's-Your-Name?.py
+++ Python Challenges/What's-Your-Name?.py
@@ -1,10 +1,8 @@
 '''
 You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
-
 Hello firstname lastname! You just delved into python.
 
 Function Description
-
 Complete the print_full_name function in the editor below.
 
 print_full_name has the following parameters:
