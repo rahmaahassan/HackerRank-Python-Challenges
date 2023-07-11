493fac27c03f0e269d675c78b8521efea59c0007
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-07-11 12:55:18 +0300

    Update What's-Your-Name?.py

--- Python Challenges/What's-Your-Name?.py
+++ Python Challenges/What's-Your-Name?.py
@@ -12,3 +12,11 @@ print_full_name has the following parameters:
 string first: the first name
 string last: the last name
 '''
+
+def print_full_name(first, last):
+    print('Hello ' + first + ' ' + last + '!' + ' ' + 'You just delved into python.')
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
