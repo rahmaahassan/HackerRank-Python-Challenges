2c7c34193003d7d1c156df17431ded001e599e94
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-08-13 12:21:14 +0300

    Update lists.py

--- Python Challenges/lists.py
+++ Python Challenges/lists.py
@@ -9,3 +9,26 @@ sort: Sort the list.
 pop: Pop the last element from the list.
 reverse: Reverse the list.
 '''
+
+if __name__ == '__main__':
+    N = int(input())
+    
+    list = []
+    for _ in range(N):
+        commend = input().split()
+        
+        if commend[0] == 'insert':
+            list.insert(int(commend[1]), int(commend[2]))
+        elif commend[0] == 'print':
+            print(list)
+        elif commend[0] == 'remove':
+            list.remove(int(commend[1]))
+        elif commend[0] == 'append':
+            list.append(int(commend[1]))
+        elif commend[0] == 'sort':
+            list.sort()
+        elif commend[0] == 'pop':
+            list.pop()
+        elif commend[0] == 'reverse':
+            list.reverse()
+        
